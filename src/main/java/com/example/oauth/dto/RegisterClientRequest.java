7f7c550017acf072fad52c349894f2ff535a1ded
Rohit Rai <rohitraijob@gmail.com>
2024-02-29 23:11:13 +0530

    Add RegisterClientResponse dto

--- src/main/java/com/example/oauth/dto/RegisterClientRequest.java
+++ src/main/java/com/example/oauth/dto/RegisterClientRequest.java
@@ -1,6 +1,10 @@
 package com.example.oauth.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
