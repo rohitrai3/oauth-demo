cc6558f56ab6bb68c725fbca684d9472d98c0ddd
Rohit Rai <rohitraijob@gmail.com>
2024-02-29 23:12:57 +0530

    Add register endpoint in client controller

--- src/main/java/com/example/oauth/controller/ClientController.java
+++ src/main/java/com/example/oauth/controller/ClientController.java
@@ -1,9 +1,20 @@
 package com.example.oauth.controller;
 
+import com.example.oauth.dto.RegisterClientRequest;
+import com.example.oauth.dto.RegisterClientResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    @PostMapping("/register")
+    public RegisterClientResponse registerClient(@RequestBody RegisterClientRequest request) {
+
+        return null;
+    }
+
 }
