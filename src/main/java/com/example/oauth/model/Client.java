bedb64dc737d5035006d6cd11d16f4e14cf969e7
Rohit Rai <rohitraijob@gmail.com>
2024-03-01 10:56:48 +0530

    Add Client model

---
+++ src/main/java/com/example/oauth/model/Client.java
@@ -0,0 +1,39 @@
+package com.example.oauth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    private String clientId;
+    @Column
+    private String clientSecret;
+    @Column
+    private String[] redirectionURIs;
+    @Column
+    private String applicationName;
+    @Column
+    private String website;
+    @Column
+    private String description;
+    @Column
+    private String logoImagePath;
+    @Column
+    private boolean acceptanceOfLegalTerms;
+
+}
