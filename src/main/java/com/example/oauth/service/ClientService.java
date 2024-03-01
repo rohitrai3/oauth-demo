be453ce226bf418da19a3403333131116d5bb4c4
Rohit Rai <rohitraijob@gmail.com>
2024-03-01 18:38:54 +0530

    Add Client service

--- src/main/java/com/example/oauth/service/ClientService.java
+++ src/main/java/com/example/oauth/service/ClientService.java
@@ -1,4 +1,10 @@
 package com.example.oauth.service;
 
+import com.example.oauth.dto.RegisterClientRequest;
+import com.example.oauth.dto.RegisterClientResponse;
+
 public interface ClientService {
+
+    public RegisterClientResponse registerClient(RegisterClientRequest request);
+
 }
---
+++ src/main/java/com/example/oauth/service/impl/ClientServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.oauth.service.impl;
+
+import com.example.oauth.dto.RegisterClientRequest;
+import com.example.oauth.dto.RegisterClientResponse;
+import com.example.oauth.model.Client;
+import com.example.oauth.repository.ClientRepository;
+import com.example.oauth.service.ClientService;
+import com.example.oauth.util.IMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    IMapper<RegisterClientRequest, Client> registerClientRequestToClientMapper;
+
+    @Override
+    public RegisterClientResponse registerClient(RegisterClientRequest request) {
+        String clientId = UUID.randomUUID().toString();
+        String clientSecret = UUID.randomUUID().toString();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedClientSecret = passwordEncoder.encode(clientSecret);
+
+        Client client = registerClientRequestToClientMapper.map(request);
+        client.setClientId(clientId);
+        client.setClientSecret(encodedClientSecret);
+
+        Client savedClient = clientRepository.save(client);
+
+        return RegisterClientResponse.builder().clientId(savedClient.getClientId()).clientSecret(clientSecret).build();
+    }
+
+}
