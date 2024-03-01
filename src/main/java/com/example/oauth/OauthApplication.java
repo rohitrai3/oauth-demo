a35c05784111454fd70337d583a54f6ada879a8f
Rohit Rai <rohitraijob@gmail.com>
2024-03-02 00:32:52 +0530

    Enable global authentication

--- src/main/java/com/example/oauth/OauthApplication.java
+++ src/main/java/com/example/oauth/OauthApplication.java
@@ -2,8 +2,10 @@ package com.example.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 
 @SpringBootApplication
+@EnableGlobalAuthentication
 public class OauthApplication {
 
 	public static void main(String[] args) {
