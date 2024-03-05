7358d01dd4626b1d4a1ce9824fcaddba88218869
Rohit Rai <rohitraijob@gmail.com>
2024-03-05 19:25:06 +0530

    Add authorize service definition

---
+++ src/main/java/com/example/oauth/service/AuthorizeService.java
@@ -0,0 +1,10 @@
+package com.example.oauth.service;
+
+import com.example.oauth.dto.AuthorizeRequest;
+import org.springframework.web.servlet.view.RedirectView;
+
+public interface AuthorizeService {
+
+    RedirectView authorize(AuthorizeRequest request);
+
+}
